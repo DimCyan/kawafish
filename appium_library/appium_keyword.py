27520b5b586a9e9d90d9c9a42255657ef9e07d66
simon <heyyooman@163.com>
2021-07-17 17:09:33 +0800

    update appium keywords

--- appium_library/appium_keyword.py
+++ appium_library/appium_keyword.py
@@ -12,3 +12,12 @@ def remote(config_path, port='http://127.0.0.1:4723/wd/hub'):
 class AppiumKey:
     def __init__(self, port, config_path):
         self.driver = remote(port, config_path)
+
+    def sleep(self, txt):
+        time.sleep(txt)
+
+    def locator(self, name, value):
+        self.driver.find_element(name, value)
+
+    def quit(self):
+        self.driver.quit()
