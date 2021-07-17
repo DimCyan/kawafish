47056156862ad38d4448ca4a427750afeb33712d
simon <heyyooman@163.com>
2021-07-17 17:01:24 +0800

    update config function

--- appium_library/appium_keyword.py
+++ appium_library/appium_keyword.py
@@ -1,12 +1,14 @@
 import time
 from appium import webdriver
+from yaml_driver.yaml_read import get_caps
 
 
-def remote(config):
-    driver = webdriver.Remote(config)
+def remote(config_path, port='http://127.0.0.1:4723/wd/hub'):
+    desired_caps = get_caps(config_path)
+    driver = webdriver.Remote(port, desired_caps)
     return driver
 
 
 class AppiumKey:
-    def __init__(self, config):
-        self.driver = remote(config)
+    def __init__(self, port, config_path):
+        self.driver = remote(port, config_path)
--- yaml_driver/yaml_read.py
+++ yaml_driver/yaml_read.py
@@ -1,1 +1,11 @@
-# import pyyaml
+import yaml
+
+
+def get_caps(config_path='../config/android_config.yml'):
+    with open(config_path, 'r') as f:
+        y = yaml.load(f, Loader=yaml.SafeLoader)
+        return y
+
+
+if __name__ == '__main__':
+    get_caps()
