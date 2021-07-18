4ce87adb5f658a4f13e1b1198a4fb1599ea8f18b
simon <heyyooman@163.com>
2021-07-18 10:32:33 +0800

    update appium init method

--- appium_library/appium_keyword.py
+++ appium_library/appium_keyword.py
@@ -3,14 +3,14 @@ from appium import webdriver
 from yaml_driver.yaml_read import get_caps
 
 
-def remote(config_path, port='http://127.0.0.1:4723/wd/hub'):
+def remote(config_path='../config/android_config.yml', port='http://127.0.0.1:4723/wd/hub'):
     desired_caps = get_caps(config_path)
     driver = webdriver.Remote(port, desired_caps)
     return driver
 
 
 class AppiumKey:
-    def __init__(self, port, config_path):
+    def __init__(self, port=None, config_path=None):
         self.driver = remote(port, config_path)
 
     def sleep(self, txt):
